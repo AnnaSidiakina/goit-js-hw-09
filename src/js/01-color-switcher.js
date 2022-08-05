047f3941cdee5efe5c7067e602a0cbc7e0c8e002
AnnaSidiakina <fruktik@gmail.com>
2022-08-05 23:34:58 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+const INTERVAL_DELAY = 1000;
 
+bodyEl.classList.add('body__task1');
+startBtn.classList.add('button');
+stopBtn.classList.add('button');
+
+let timerId = null;
+
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', colorChange);
+stopBtn.addEventListener('click', onStopBtn);
+
+function colorChange() {
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DELAY);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onStopBtn() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
