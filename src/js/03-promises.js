28ffeaab39b3ec97796f7a8e96b1e937616371d4
AnnaSidiakina <fruktik@gmail.com>
2022-08-08 15:49:15 +0200

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+let delayInput = null;
+let stepInput = null;
+let amountInput = null;
+
+formEl.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+        // Fulfill
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  delayInput = Number(delay.value);
+  stepInput = Number(step.value);
+  amountInput = Number(amount.value);
+
+  for (let i = 1; i <= amountInput; i++) {
+    createPromise(i, delayInput)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayInput += stepInput;
+    // console.log(delayInput);
+    // console.log(amountInput);
   }
+  event.currentTarget.reset();
 }
